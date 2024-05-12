3f7a2ee1404eb28ef561280da2e0261ac8aa94fb
nicolezeg <nzegarra07@gmail.com>
2024-05-12 00:39:38 -0500

    Week 02 - complete

---
+++ scripts/temples.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+});
+
+const closeBtn = document.querySelector('.close-btn');
+
+closeBtn.addEventListener('click', () => {
+  navMenu.classList.remove('show');
+});
+
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").innerText = currentYear;
+
+// Get the last modified date of the document
+const lastModifiedDate = document.lastModified;
+document.getElementById("lastModified").innerText = "Last Modified: " + lastModifiedDate;
